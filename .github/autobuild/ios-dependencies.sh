fdae3db0f2498add06d252767807b6bcedb3e8aa
Peter L Jones <peter.l.jones.dymlyd@gmail.com>
2026-08-23 16:31:25 +0100

    Externalise cache keys to prevent unneeded invalidation

---
+++ .github/autobuild/ios-dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Values are consumed by ios.sh and the dependency cache key.
+# shellcheck disable=SC2034
+export AQTINSTALL_VERSION="${AQTINSTALL_VERSION:-3.3.0}"
+export QT_VERSION="${QT_VERSION:-5.15.2}"
---
+++ .github/autobuild/mac-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Values are consumed by mac.sh and the dependency cache key.
+# shellcheck disable=SC2034
+export AQTINSTALL_VERSION="${AQTINSTALL_VERSION:-3.3.0}"
+export QT_VERSION="${QT_VERSION:-6.10.2}"
+export QT_LEGACY_VERSION=5.15.2
