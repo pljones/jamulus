b54fb6f6c4304c1e010588f7b414089b4fa23575
Peter L Jones <peter.l.jones.dymlyd@gmail.com>
2026-08-23 18:29:02 +0100

    Rewrite Android build process

---
+++ .github/autobuild/android-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Values are consumed by scripts which source this file.
+# shellcheck disable=SC2034
+export COMMANDLINETOOLS_VERSION=6858069
+export ANDROID_NDK_VERSION=r21d
+export ANDROID_PLATFORM=android-30
+export ANDROID_BUILD_TOOLS=30.0.2
+export AQTINSTALL_VERSION="${AQTINSTALL_VERSION:-3.3.0}"
+export QT_VERSION="${QT_VERSION:-5.15.2}"
