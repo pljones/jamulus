833dc0d1df917c15539eea4906e0efdb1ad0e7e1
Peter L Jones <peter.l.jones.dymlyd@gmail.com>
2026-08-28 13:29:03 +0100

    Externalise cache keys to prevent unneeded invalidation

---
+++ .github/autobuild/windows-dependencies.ps1
@@ -0,0 +1,19 @@
+# The following version pinnings are semi-automatically checked for updates.
+# Verify .github/workflows/bump-dependencies.yaml when changing those manually:
+
+# Values are consumed by .github/autobuild/windows.ps1, windows/deploy_windows.ps2 and the dependency cache key.
+$Qt32Version = "5.15.2"
+$Qt64Version = "6.8.1"
+$QtCompile32 = "msvc2019"
+$QtCompile64 = "msvc2022"
+$AqtinstallVersion = "3.3.0"
+$JackVersion = "1.9.22"
+$JomVersion = "1.1.2"
+
+# Important:
+# - Do not update ASIO SDK without checking for license-related changes.
+# - Do not copy (parts of) the ASIO SDK into the Jamulus source tree without
+#   further consideration as it would make the license situation more complicated.
+$AsioSDKVersion = "2.3.4_2025-10-15"
+
+$NsisVersion = "3.12"
