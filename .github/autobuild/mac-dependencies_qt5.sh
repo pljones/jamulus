c5fd107f81937255fbbe53d309a779dcf4414c7f
Peter L Jones <peter.l.jones.dymlyd@gmail.com>
2026-08-29 14:44:36 +0100

    Externalise cache keys to prevent unneeded invalidation

---
+++ .github/autobuild/mac-dependencies_qt5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Values are consumed by .github/autobuild/mac.sh, mac/deploy-mac.sh and the dependency cache key.
+# shellcheck disable=SC2034
+AQTINSTALL_VERSION=3.3.0
+QT_VERSION=5.15.2
+CREATEDMG_VERSION=1.3.0
